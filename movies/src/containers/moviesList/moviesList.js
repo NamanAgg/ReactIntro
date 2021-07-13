00e12b7fe121a8d1d3afea4724d9f3c184e13c1a
naman <namanagg.2010@gmail.com>
2021-07-13 19:42:00 +0530

    update

--- movies/src/containers/moviesList/moviesList.js
+++ movies/src/containers/moviesList/moviesList.js
@@ -14,13 +14,17 @@ class MoviesList extends React.Component {
         this.state = {
             search: "",
             pageNumber: 1,
-            rating: "all"
+            "rating": "all",
+            data:[]
         };
     }
 
     componentDidMount() {
-        axios.get("https://react-backend101.herokuapp.com/movies").then((res) => {
-            console.log(res);
+        axios.get("https://apimovies.free.beeceptor.com/movies").then((res) => {
+            console.log(res.data);
+            this.setState({
+              data: res.data
+            });
         })
     }
 
@@ -40,133 +44,12 @@ class MoviesList extends React.Component {
     changeRating = (e) => {
         this.setState({
             pageNumber: 1,
-            rating: e.target.value === "all" ? "all" : parseInt(e.target.value)
+            "rating": e.target.value === "all" ? "all" : parseInt(e.target.value)
         })
     }
 
     render() {
-        let data = [{
-            sno: 1,
-            name: "P.K",
-            genre: "Comedy",
-            rating: 4
-        },
-        {
-            sno: 2,
-            name: "Sholay",
-            genre: "Drama",
-            rating: 5
-        },
-
-        {
-            sno: 3,
-            name: "Anand",
-            genre: "Comedy",
-            rating: 3
-        },
-        {
-            sno: 4,
-            name: "Harry potter",
-            genre: "Fictional",
-            rating: 2
-        },
-        {
-            sno: 5,
-            name: "Hungama",
-            genre: "Comedy",
-            rating: 4
-        },
-        {
-            sno: 6,
-            name: "Jokar",
-            genre: "Emotional",
-            rating: 5
-        },
-        {
-            sno: 7,
-            name: "Tanhaji",
-            genre: "Action",
-            rating: 3
-        },
-        {
-            sno: 8,
-            name: "Aaj kal",
-            genre: "Romantic",
-            rating: 0
-        },
-        {
-            sno: 9,
-            name: "Golmaal",
-            genre: "Comedy",
-            rating: 4
-        },
-        {
-            sno: 10,
-            name: "Angrezi medium",
-            genre: "Comedy",
-            rating: 1
-        },
-        {
-            sno: 11,
-            name: "Race 3",
-            genre: "Against Physics",
-            rating: 5
-        },
-        {
-            sno: 12,
-            name: "ABCD",
-            genre: "Dance",
-            rating: 2
-        },
-        {
-            sno: 13,
-            name: "Happy new year",
-            genre: "Dance",
-            rating: 3
-        },
-        {
-            sno: 14,
-            name: "Kaal",
-            genre: "Fictional",
-            rating: 4
-        },
-        {
-            sno: 15,
-            name: "DDLJ",
-            genre: "Romantic",
-            rating: 1
-        },
-        {
-            sno: 16,
-            name: "Article 15",
-            genre: "Crime drama",
-            rating: 0
-        },
-        {
-            sno: 17,
-            name: "Special 26",
-            genre: "Action",
-            rating: 2
-        },
-        {
-            sno: 18,
-            name: "Padman",
-            genre: "Drama",
-            rating: 4
-        },
-        {
-            sno: 19,
-            name: "Baby",
-            genre: "Funny",
-            rating: 3
-        },
-        {
-            sno: 20,
-            name: "Badla",
-            genre: "Drama crime",
-            rating: 5
-        }
-        ];
+        let data =this.state.data;
 
         let filteredData = data.filter((movie) => {
             if (this.state.rating !== "all") {
