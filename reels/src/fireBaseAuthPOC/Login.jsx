8564712df2d7e276809b0278fc8c672f27f18054
nmn <namanagg.2010@gmail.com>
2021-08-03 23:52:53 +0530

    update

--- reels/src/fireBaseAuthPOC/Login.jsx
+++ reels/src/fireBaseAuthPOC/Login.jsx
@@ -1,38 +1,96 @@
 import React, {useState} from'react';
 import firebase from './firebase'
 //authentication use
+ //email, password -> firebase 
+       
 const auth=firebase.auth();
 function Login(){
-    let [email,setEmail] = useState("");
-    let [password,setPassword] = useState("");
-    const LoginFn = async()=>{
-        //email, password -> firebase 
-        // **********firebase console
+     // **********firebase console
         //auth start
         //enable->firebase console
         //user create on firbase side
 
         //npm i firebase -> local machine
         //firebase login
-        let res = await auth.signInWithEmailAndPassword(email,password);
-        //unique id
-        console.log(res.user.uid); 
+    let [email, setEmail] = useState("");
+    let [password, setPassword] = useState("");
+    let [userId, setuserId] = useState("");
+    let [loginLoder, setLoginLoader] = useState(false);
+    let [error, setError] = useState("");
+    const loginFn = async () => {
+        try {
+            setLoginLoader(true);
+            let res = await auth.signInWithEmailAndPassword(email, password);
+            // unique id
+            console.log(res.user.uid);
+            setLoginLoader(false)
+            setuserId(res.user.uid)
+        } catch (err) {
+            setLoginLoader(false);
+            setError(err.message);
+            setTimeout(() => {
+                setError(false);
+            }, 2000);
+        }
     }
+    const logoutFn = async () => {
+        try {
+            setLoginLoader(true);
+            await auth.signOut();            // unique id
+            setLoginLoader(false)
+            setuserId("");
+        } catch (err) {
+            setLoginLoader(false);
+            setError(err.message);
+            setTimeout(() => {
+                setError(false);
+            }, 2000);
+        }
+    }
+    // useEffect will only run once 
+    // eventlistener -> attach 
+    // -> auth state change
+    //  -> 
+    // component did mount 
+    useEffect(() => {
+        console.log("useEffect ran");
+        function action(user) {
+            console.log(user);
+            let finalUser = user ? user.uid : null
+            setuserId(finalUser);
+        }
+        // listener -> state change auth -> login logout 
+        auth.onAuthStateChanged(action);
+    }, [])
 
-    return(
-        <div>
-            <div>
-                Email
-                <input type="email" value={email} placeholder="Enter your Email" onChange={function(e){ setEmail(e.target.value)}} />
-            </div>
-            <div>
-                Password
-                <input type="email" value={password} placeholder="**********" onChange={function(e){ setPassword(e.target.value)}} />
-            </div>
-            <div>
-                <button onClick={LoginFn}>Login</button>
-            </div>
-        </div>
+    return (
+        // form will available
+        loginLoder == true ? <h1>Loading....</h1>
+            : userId ?
+                <>
+                    <h1>{userId}
+                    </h1>
+                    <button
+                        onClick={logoutFn}
+                    >Logout</button>
+                </>
+                : <div>
+                    <div> Email:
+                        <input type="Email" placeholder="Your Email" value={email}
+                            onChange={function (e) {
+                                // logic
+                                setEmail(e.target.value)
+                            }} />
+                    </div>
+                    <div>Password:
+                        <input type="password" placeholder="********" value={password} onChange={(e) => {
+                            setPassword(e.target.value)
+                        }}
+                        />
+                    </div>
+                    <button onClick={loginFn}>Login</button>
+                    {error ? <h1>{error}</h1> : <></>}
+                </div >
     )
 }
 export default Login;
