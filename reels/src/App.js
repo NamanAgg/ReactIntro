6f4972d160c6d57fd0e38178967322245c4c48ce
nmn <namanagg.2010@gmail.com>
2021-08-04 17:33:09 +0530

    update

--- reels/src/App.js
+++ reels/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Login from "./fireBaseAuthPOC/Login.jsx";
+import Todo from "./useEffectExample/Todo";
 function App() {
   return (
   <div>
- <Login />
+  {/* <Login></Login> */}
+  <Todo></Todo>
   </div>
   );
 }
